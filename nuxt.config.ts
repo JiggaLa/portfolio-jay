a93c5bd9a85d73334c1aaa22d768e0549d1692a4
JiggaLa <la.japh@gmail.com>
2022-02-26 09:34:45 +0200

    feat: add fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import postcssOptions from "./postcss.config.js";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  css: ["@/assets/css/main.css"],
+  css: ["@/assets/css/main.css", "@/assets/css/3-basics/base.css"],
 
   build: {
     postcss: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,30 @@ module.exports = {
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Gilroy, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
+    },
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+    },
+    extend: {
+      colors: {
+        "pj-black": "#0f0f0f",
+      },
+    },
   },
   plugins: [],
 };
