19cb498b2c947d0b62c28d80ac1a34d9828b1991
JiggaLa <la.japh@gmail.com>
2022-03-02 23:19:13 +0200

    feat: add bio section

--- app.vue
+++ app.vue
@@ -19,5 +19,63 @@
         Japheth
       </span>
     </section>
+    <section
+      class="flex flex-col items-center overflow-hidden mt-[13%] xl:w-full"
+    >
+      <span
+        class="text-[2.2rem] text-white/50 lg:text-[1.6rem] md:text-[1.4rem]"
+        >Biography</span
+      >
+
+      <p
+        class="text-[2.2rem] text-center w-[807px] mt-6 lg:w-[60%] lg:text-[1.6rem] md:w-[80%] md:text-[1.4rem]"
+      >
+        Work for money and design and develop for love! I'm Japheth, a UX/UI
+        designer and front-end developer based in Cyprus.
+      </p>
+
+      <div
+        class="w-full relative z-[100] overflow-x-auto grid grid-flow-col auto-cols-auto gap-[40px] grid-c-content justify-start items-start content-start mt-24 xl:pl-[10vw] xl:pr-16 md:overflow-hidden md:w-[90%] md:px-0 md:auto-cols-[100%] md:grid-flow-row"
+      >
+        <div class="ibox w-[300px] md:w-full md:items-center">
+          <span class="text-[1.1rem] text-white/50">Contact</span>
+          <p class="text-[1.3rem] leading-[1.5] md:text-center">
+            Ammochostos, Cyprus<br />la.japh@gmail.com<br />+905338433770
+          </p>
+        </div>
+
+        <div class="ibox items-center">
+          <span class="text-[1.1rem] text-white/50 text-center"
+            >Years of<br />Experience</span
+          >
+          <span class="text-8xl leading-[0.8] font-semibold mt-4">7</span>
+        </div>
+
+        <div
+          class="ibox items-center bg-center bg-cover d-bg w-[327px] h-[530px] md:w-full md:h-[430px]"
+        >
+          <span class="text-[1.1rem] text-white/50 text-center"
+            >Guiding principle</span
+          >
+          <p class="text-[1.3rem] leading-[1.5] mt-4 text-center">
+            Design-driven<br />problem solving
+          </p>
+        </div>
+
+        <div class="h-full w-[300px] md:w-full">
+          <div class="ibox w-full items-center">
+            <span class="text-[1.1rem] text-white/50 text-center"
+              >Years of<br />Experience</span
+            >
+          </div>
+
+          <p class="mt-[40%] text-[1.5rem] md:mt-[10%]">
+            I'm a passionate designer and developer with many years of
+            experience who uses data, research, and thoughtful designs to create
+            products that scale.
+          </p>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
