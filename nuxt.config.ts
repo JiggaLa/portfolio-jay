36ef132466e73e5cc3d573e6873777c419fc77fd
JiggaLa <la.japh@gmail.com>
2022-03-03 03:48:19 +0200

    feat: add site meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,44 @@ import postcssOptions from "./postcss.config.js";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  meta: {
+    title: "Portfolio Jay",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Work for money and design and develop for love! I'm Japheth, a UX/UI designer and front-end developer based in Cyprus.",
+      },
+      { name: "format-detection", content: "telephone=no" },
+      { name: "msapplication-TileColor", content: "#0f0f0f" },
+      {
+        name: "theme-color",
+        media: "(prefers-color-scheme: light)",
+        content: "#0f0f0f",
+      },
+      {
+        name: "theme-color",
+        media: "(prefers-color-scheme: dark)",
+        content: "#030307",
+      },
+    ],
+    link: [
+      { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+      {
+        rel: "apple-touch-icon",
+        href: "/pwa-192x192.png",
+      },
+      {
+        rel: "mask-icon",
+        href: "/safari-pinned-tab.svg",
+        color: "#0f0f0f",
+      },
+    ],
+  },
+
   css: ["@/assets/css/main.css", "@/assets/css/3-basics/base.css"],
 
   build: {
